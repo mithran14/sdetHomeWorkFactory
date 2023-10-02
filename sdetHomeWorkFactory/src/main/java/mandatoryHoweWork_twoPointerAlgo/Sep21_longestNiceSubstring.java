792edca9d7db8d599f6bb434f108addc0d91b35f
Mari Vignesh <mtm.vignesh.js@gmail.com>
2023-10-02 06:41:08 +0530

    21/09/2023_new

--- sdetHomeWorkFactory/src/main/java/mandatoryHoweWork_twoPointerAlgo/Sep21_longestNiceSubstring.java
+++ sdetHomeWorkFactory/src/main/java/mandatoryHoweWork_twoPointerAlgo/Sep21_longestNiceSubstring.java
@@ -1,44 +1,51 @@
 package mandatoryHoweWork_twoPointerAlgo;
 
-import java.util.HashSet;
-import java.util.Set;
-import org.junit.Assert;
 import org.junit.Test;
+import org.testng.Assert;
 
 public class Sep21_longestNiceSubstring {
 
-	/*
-	 *https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
-	 * 
-	#i/p String s, o/p int totalCount
-	#length of the input string s is less than 2,  returns an empty string "".
-	# initializes a HashSet called set to store unique characters 
-	#using for each loop input string s and adds it to the set. 
-	#using for loop check, For each character at index i, it checks if both the uppercase and lowercase 
-	#if yes then continue, else calls the longestNiceSubstring function
-		s.substring(0, i): the substring of s from the 0 to index i
-		s.substring(i + 1): substring of s from index i + 1 to the end.
-	#lengths of the two substrings obtained  and returns the longer of the two (str1 or str2).
-	#keep dividing the string into smaller substrings, searching for the longest "Nice" substring.
-	#If no further division returns the original string s.
-	 */
+	//ip : Stri g
+	//op : String
+	//Logic init  empty string ans
+	//1. Run two for loops, from i=0 & j=i+1 till length of string, take subString using i and j (j+1)
+	//2. Check if the sub isNice and len of sub >len of ans, if yes assign the sub to ans
+	//3. Return ans
 
-	 public String longestNiceSubstring(String s) {
-	        if (s.length() < 2)
-	            return "";
-	        Set<Character> set = new HashSet<>();
-	        for (char ch : s.toCharArray())
-	            set.add(ch);
-	        for (int i=0; i<s.length(); i++) {
-	            char current = s.charAt(i);
-	            if (set.contains(Character.toUpperCase(current)) && set.contains(Character.toLowerCase(current)))
-	                continue;
-	            String str1 = longestNiceSubstring(s.substring(0, i));
-	            String str2 = longestNiceSubstring(s.substring(i + 1));
-	            return str1.length() >= str2.length() ? str1 : str2;
-	        }
-	        return s;
-	    }
+	//IsNice
+	//Run a for each by converting the passed substring to char[]
+	//if char is upper convert to lower and vice versa - store in char
+	// if the converted char is not present in the substring passed, return false
+	//return true in all other cases
+
+	//Time complexity - O[N3]
+
+	public String longestNiceSubstring(String s) {
+
+		String ans = "";
+
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i + 1; j < s.length(); j++) {
+				String sub = s.substring(i, j + 1);
+				if (isNice(sub) && sub.length() > ans.length()) {
+					ans = sub;
+				}
+			}
+		}
+
+		return ans;
+	}
+
+	public static boolean isNice(String s) {
+		for (char c : s.toCharArray()) {
+			char op = Character.isUpperCase(c) ? Character.toLowerCase(c) : Character.toUpperCase(c);
+			if (s.indexOf(op) == -1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	    
 	@Test
 	public void TwoPositive0() {
