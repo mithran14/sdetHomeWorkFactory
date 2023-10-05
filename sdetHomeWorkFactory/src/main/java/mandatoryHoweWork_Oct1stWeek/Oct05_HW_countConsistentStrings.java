b18cfcf7d57f76ad2c075737b494e883e1f1f19d
Mari Vignesh <30817381+mithran14@users.noreply.github.com>
2023-10-05 22:58:28 +0530

    Add files via upload

---
+++ sdetHomeWorkFactory/src/main/java/mandatoryHoweWork_Oct1stWeek/Oct05_HW_countConsistentStrings.java
@@ -0,0 +1,43 @@
+package mandatoryHoweWork_Oct1stWeek;
+
+import java.util.HashSet;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+public class Oct05_HW_countConsistentStrings{
+
+	public int countConsistentStrings(String allowed, String[] words) {
+		int count=0,totCount=0;
+		HashSet<Character> all=new HashSet<Character>();
+		for (char c : allowed.toCharArray()) {
+			all.add(c);
+		}
+		for (String s : words) {
+			for (int i = 0; i < s.length(); i++) {
+				if (all.contains(s.charAt(i))) {
+					count++;
+				}
+			}
+			if (count==s.length()) {
+				totCount++;
+			}
+			count=0;
+		}
+		return totCount;
+	}
+	@Test
+	public void countConsistentStringsPositive()
+	{
+		int actual=countConsistentStrings("ab",new String[] {"ad","bd","aaab","baa","badab"});
+		int expected =2;
+		Assert.assertEquals(expected, actual);
+	}
+	@Test
+	public void countConsistentStringsPositive1()
+	{
+		int actual=countConsistentStrings("abc",new String[] {"a","b","c","ab","ac","bc","abc"});
+		int expected =7;
+		Assert.assertEquals(expected, actual);
+	}
+}
---
+++ sdetHomeWorkFactory/src/main/java/mandatoryHoweWork_Oct1stWeek/Oct05_HW_sumOfUnique.java
@@ -0,0 +1,43 @@
+package mandatoryHoweWork_Oct1stWeek;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import org.junit.Test;
+import org.testng.Assert;
+
+public class Oct05_HW_sumOfUnique{
+
+	public int sumOfUnique(int[] nums) {
+		int sum=0,sum1=0;
+		Set<Integer> orgLst=new HashSet<Integer>();
+		Set<Integer> dupLst=new HashSet<Integer>();
+		for(int i=0;i<nums.length;i++){
+			if(!orgLst.contains(nums[i])){
+				orgLst.add(nums[i]);
+				sum+=nums[i];
+			}
+			else{
+				dupLst.add(nums[i]);
+			}
+		}
+		Iterator<Integer> iterator = dupLst.iterator();
+		while (iterator.hasNext()) {
+			sum1+= iterator.next();
+		}
+		return sum-sum1;
+	}
+	@Test
+	public void sumOfUniquePositive()
+	{
+		int actual=sumOfUnique(new int[] {1,2,3,2});
+		int expected =4;
+		Assert.assertEquals(expected, actual);
+	}
+	public void sumOfUniquePositive1()
+	{
+		int actual=sumOfUnique(new int[] {1,2,3,2,1,2});
+		int expected =3;
+		Assert.assertEquals(expected, actual);
+	}
+}
